a9d22ad564e5edb490933385cd830b7328dacd2b
LaelBelchior <laelbelchior@gmail.com>
2022-03-19 14:07:18 -0300

    Aprendendo a manipular array com map, usort e reduce

---
+++ manipulandoarray.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'dados.php';
+
+$numeroDePaises = count($dadosEmJson);
+echo "Número de países participantes: $numeroDePaises";
+
+$dadosEmJson = array_map(function ($item){
+    $item['pais'] = mb_convert_case($item['pais'], CASE_UPPER);
+}, $dadosEmJson);
+
+usort($dadosEmJson, function ($item1, $item2) {
+    $medalhas1 = $item1['medalhas'];
+    $medalhas2 = $item2['medalhas'];
+
+    return $medalhas2['ouro'] - $medalhas1['ouro'] !== 0
+        ? $medalhas2['ouro'] - $medalhas1['ouro']
+        : ($medalhas2['prata'] - $medalhas1['prata'] !== 0
+            ? $medalhas2['prata'] - $medalhas1['prata'] 
+            : $medalhas2['bronze'] - $medalhas1['bronze']);
+
+});
+
+$numeroDeMedalhas = array_reduce($dadosEmJson, function ($medalhasAcumuladas, $itemAtual) {
+    $medalhasDoPais = array_reduce($itemAtual['medalhas'], function ($medalhasAcumuladasDoPaisAtual, $medalhasDoPaisAtual) {
+        return $medalhasAcumuladasDoPaisAtual + $medalhasDoPaisAtual;
+    }, 0);
+
+    return $medalhasAcumuladas + $medalhasDoPais;
+}, 0);
+
+echo "Total de medalhas entregues: $numeroDeMedalhas";
+
+
+
